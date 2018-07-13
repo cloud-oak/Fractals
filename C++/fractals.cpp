f6ea4c46a396a0953b3814ba4f01bf6d999ffbcb
Konrad Heidler <heidler@itestra.de>
2018-07-13 20:02:50 +0200

    Why would anyone ever indent with tabs? o.O

--- C++/fractals.cpp
+++ C++/fractals.cpp
@@ -32,82 +32,82 @@ SDL_Event event;
 
 void calcRange(int minX, int maxX)
 {
-	for(int x = minX; x<maxX; x++) {
-		for(int y = 0; y<HEIGHT; y ++) {
-			complex<double> c = {double(x) / WIDTH  * (MAXX-MINX) + MINX,
-								 double(y) / HEIGHT * (MAXY-MINY) + MINY};
-			complex<double> z = c;
+  for(int x = minX; x<maxX; x++) {
+    for(int y = 0; y<HEIGHT; y ++) {
+      complex<double> c = {double(x) / WIDTH  * (MAXX-MINX) + MINX,
+                 double(y) / HEIGHT * (MAXY-MINY) + MINY};
+      complex<double> z = c;
                         iters[x][y] = 0;
-			for(iters[x][y] = 0; iters[x][y] < MAX_ITERATIONS && abs(z) < 4; iters[x][y]++) {
-				z = z*z + c;
-			}
-		}
-	}
+      for(iters[x][y] = 0; iters[x][y] < MAX_ITERATIONS && abs(z) < 4; iters[x][y]++) {
+        z = z*z + c;
+      }
+    }
+  }
 
 }
 
 int main()
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("Mandelbrot", SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  SDL_Init(SDL_INIT_VIDEO);
+  window = SDL_CreateWindow("Mandelbrot", SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	bool quit = false;
-	bool invalid = true;
+  bool quit = false;
+  bool invalid = true;
 
-	while (!quit)
-	{
-		while (SDL_PollEvent(&event) > 0) {
-			if (event.type == SDL_QUIT)
-				quit = true;
-			if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == 1) {
-					float nWidthHalf = (MAXX - MINX) / 4;
-					float nHeightHalf = (MAXY - MINY) / 4;
-					float nCenterX = double(event.button.x) / WIDTH  * (MAXX-MINX) + MINX;
-					float nCenterY = double(event.button.y) / HEIGHT * (MAXY-MINY) + MINY;
-					MINX = nCenterX - nWidthHalf;
-					MAXX = nCenterX + nWidthHalf;
-					MINY = nCenterY - nHeightHalf;
-					MAXY = nCenterY + nHeightHalf;
+  while (!quit)
+  {
+    while (SDL_PollEvent(&event) > 0) {
+      if (event.type == SDL_QUIT)
+        quit = true;
+      if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == 1) {
+          float nWidthHalf = (MAXX - MINX) / 4;
+          float nHeightHalf = (MAXY - MINY) / 4;
+          float nCenterX = double(event.button.x) / WIDTH  * (MAXX-MINX) + MINX;
+          float nCenterY = double(event.button.y) / HEIGHT * (MAXY-MINY) + MINY;
+          MINX = nCenterX - nWidthHalf;
+          MAXX = nCenterX + nWidthHalf;
+          MINY = nCenterY - nHeightHalf;
+          MAXY = nCenterY + nHeightHalf;
 
-					MAX_ITERATIONS += 5;
+          MAX_ITERATIONS += 5;
 
-					invalid = true;
-				break;
-			}
-		}
+          invalid = true;
+        break;
+      }
+    }
 
-		if(invalid)
-		{
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-			SDL_RenderClear(renderer);
-			
-			for(int i = 0; i<NUM_THREADS; i++)
-			{
-				threads[i] = thread(calcRange,
-				 	i   *  WIDTH / (NUM_THREADS),
-					(i+1)* WIDTH / (NUM_THREADS));
-			}
-			for(int i = 0; i<NUM_THREADS; i++)
-			{
-				threads[i].join();
-			}
+    if(invalid)
+    {
+      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+      SDL_RenderClear(renderer);
+      
+      for(int i = 0; i<NUM_THREADS; i++)
+      {
+        threads[i] = thread(calcRange,
+           i   *  WIDTH / (NUM_THREADS),
+          (i+1)* WIDTH / (NUM_THREADS));
+      }
+      for(int i = 0; i<NUM_THREADS; i++)
+      {
+        threads[i].join();
+      }
 
-			for(int x = 0; x < WIDTH; x++) {
-				for(int y = 0; y < HEIGHT; y++) {
-					SDL_SetRenderDrawColor(renderer,
-						255 * iters[x][y] / MAX_ITERATIONS,
-						0,
-						255 - (255 / MAX_ITERATIONS)*iters[x][y],
-						255);
-					SDL_RenderDrawPoint(renderer, x, y);
-				}
-			}
-					
-			SDL_RenderPresent(renderer);
+      for(int x = 0; x < WIDTH; x++) {
+        for(int y = 0; y < HEIGHT; y++) {
+          SDL_SetRenderDrawColor(renderer,
+            255 * iters[x][y] / MAX_ITERATIONS,
+            0,
+            255 - (255 / MAX_ITERATIONS)*iters[x][y],
+            255);
+          SDL_RenderDrawPoint(renderer, x, y);
+        }
+      }
+          
+      SDL_RenderPresent(renderer);
 
-			invalid = false;
-		}
-	}
+      invalid = false;
+    }
+  }
 }
